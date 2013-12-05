39949cea621076587d09f4763b1c965fdc82c8f5
nanaprempeh009 <nanaprempeh009@gmail.com>
2013-12-05 13:39:58 +0000

    The mestly project

--- src/main/java/controller/loginServlet.java
+++ src/main/java/controller/loginServlet.java
@@ -13,58 +13,61 @@ import java.io.PrintWriter;
 import javax.servlet.http.HttpSession;
 
 
-public class LoginServlet extends HttpServlet {
+public class LoginServlet extends HttpServlet 
+{
 
-		protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-					throws ServletException, IOException {
-			req.getRequestDispatcher("/WEB-INF/login.jsp").forward(req,resp);
-		}
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+	throws ServletException, IOException
+	{
+		req.getRequestDispatcher("/WEB-INF/login.jsp").forward(req,resp);
+	}
 
 
 
-		protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-					throws ServletException, IOException { 
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+	throws ServletException, IOException 
+	{ 
 			//Getting the details of the user for authentication and loggin in
-			String username = req.getParameter("username");
-			String password = req.getParameter("password");
-	
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+
 			//Getting the username and password from the Json file saved on the server
-			String varifyusername = UserManager.getUser(username).getUsername();
-			System.out.println(varifyusername);
-			String varifypassword = UserManager.getUser(username).getPassword();
-			System.out.println(req.getParameter("next"));
+		String varifyusername = UserManager.getUser(username).getUsername();
+		System.out.println(varifyusername);
+		String varifypassword = UserManager.getUser(username).getPassword();
+		System.out.println(req.getParameter("next"));
 		
-			if ( username.equals(varifyusername) && username != "") {
-					
-				if ( password.equals(varifypassword) && password != "") {
+		if ( username.equals(varifyusername) && username != "") {
 
-					HttpSession session = req.getSession();
-					session.setAttribute("Password", password);
-					session.setAttribute("Username", username);
+			if ( password.equals(varifypassword) && password != "") {
 
-					System.out.println(req.getParameter("next"));
+				HttpSession session = req.getSession();
+				session.setAttribute("Password", password);
+				session.setAttribute("Username", username);
 
-					
+				System.out.println(req.getParameter("next"));
 
-					if (req.getParameter("next") == null) {
-							System.out.println("int the next section");
-							resp.sendRedirect("/dashboard");
-						 	
-					}else{	
-							resp.sendRedirect(req.getParameter("next"));
-							System.out.println("the next didn't work");
-					}  
-				}else{
-					String wrongpassword = "Password entered does not match the username entered";
-					req.setAttribute("Wrongpassword",wrongpassword);
-					req.getRequestDispatcher("/WEB-INF/login.jsp").forward(req,resp);
-					return;	
-				}
+
+
+				if (req.getParameter("next") == null) {
+					System.out.println("int the next section");
+					resp.sendRedirect("/dashboard");
+
+				}else{	
+					resp.sendRedirect(req.getParameter("next"));
+					System.out.println("the next didn't work");
+				}  
 			}else{
-				String wronguser = "Username entered is not registered with Mestly";
-				req.setAttribute("Wronguser",wronguser);
+				String wrongpassword = "Password entered does not match the username entered";
+				req.setAttribute("Wrongpassword",wrongpassword);
 				req.getRequestDispatcher("/WEB-INF/login.jsp").forward(req,resp);
-				return;
-			} 			
-		}
+				return;	
+			}
+		}else{
+			String wronguser = "Username entered is not registered with Mestly";
+			req.setAttribute("Wronguser",wronguser);
+			req.getRequestDispatcher("/WEB-INF/login.jsp").forward(req,resp);
+			return;
+		} 			
+	}
 }
