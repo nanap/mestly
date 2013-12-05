3cdc07847c694dbff1a8ef3719dd774d1781195c
nanaprempeh009 <nanaprempeh009@gmail.com>
2013-12-05 14:24:04 +0000

    fixDash

---
+++ src/main/java/controller/DashBoardServlet.java
@@ -0,0 +1,35 @@
+package controller;
+
+import model.mest.entity.UserManager;
+import model.mest.entity.User;
+import java.io.*;
+import java.io.IOException;
+import javax.servlet.*;
+import javax.servlet.http.*; 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+  
+
+public class DashBoardServlet extends HttpServlet{
+
+   
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+	   throws ServletException, IOException{
+
+	   	//we place the entire method in a brackets and wrote String at the front
+	   	// so that the object returned from the session can be typecasted into a string
+	   	// another way we could do that is to add .toString() at the end of the line before
+	   	// the ;
+		String username = req.getSession().getAttribute("Username").toString();	 	
+	   	String email = UserManager.getUser(username).getEmail();
+	   	//req.getSession().getAttribute("Email").toString();
+
+	   	// UserManager.getUser(currentuser).getEmail();
+	   	req.setAttribute("Username",username);
+	   	req.setAttribute("Email",email );
+	   	req.getRequestDispatcher("/WEB-INF/dashboard.jsp").forward(req,resp);
+    }
+}
+
